3836e12619f9d19c74eb4c585c465b439bd47458
Junjie Mao <junjie.mao@enight.me>
2015-09-30 10:25:12 +0800

    Hide examples in later chapters and add some bug-correcting exercises
    Signed-off-by: Junjie Mao <junjie.mao@enight.me>

--- examples/scandir.php
+++ examples/scandir.php
@@ -8,7 +8,7 @@
   while ($file = @readdir($handle)) {
     if (($tmp = strlen($file) - 4) >= 0 && strpos($file, ".txt", $tmp) !== FALSE) {
       $comment = substr(fgets(fopen($file, 'r')), 2);
-      if (strpos($comment, 'TODO') === FALSE) {
+      if (strpos($comment, 'HIDE') === FALSE) {
         array_push($files, $file . '|' . $comment);
       }
     }
